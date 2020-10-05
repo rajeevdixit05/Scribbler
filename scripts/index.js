7967a71105ff0e52ca034eaae9f9a4c961626278
Rajeev Dixit <rajeev.dixit@myntra.com>
2020-10-05 10:22:12 +0530

    add user sign up and signin modal

---
+++ scripts/index.js
@@ -0,0 +1,54 @@
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("si");
+var btn1 = document.getElementById("su");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// ****************MODAL2*********MODAL2
+var modal1 = document.getElementById("myModal1");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+// *************
+var sp=document.getElementById("redirect");
+sp.onclick=function()
+{
+  modal.style.display = "none";
+  modal1.style.display = "block";
+}
